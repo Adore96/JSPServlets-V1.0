bb71d83a52463ed2c157a4f4533d06ecb663787d
Kalhara Perera <adorekasun@gmail.com>
2020-07-29 11:18:36 +0530

    Final Commit

--- src/com/Adore96/AddServlet.java
+++ src/com/Adore96/AddServlet.java
@@ -11,38 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class AddServlet extends HttpServlet{
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	public void doPost(HttpServletRequest req , HttpServletResponse res) throws IOException, ServletException {
-		
+	
+	
+	public void doGet(HttpServletRequest req , HttpServletResponse res)throws IOException, ServletException {
+		System.out.println("DoPost called");
 		int i = Integer.parseInt(req.getParameter("num1")) ;
 		int j = Integer.parseInt(req.getParameter("num2")) ;
 		
 		int k = i+j;
-		int l = i/j;
-		int m = i*j;
-		int n = Math.abs(i-j);
 		
-		PrintWriter out= res.getWriter();
-		
-		out.println("Addition is :"+ k);
-		out.println("Substraction is :"+ n);
-		out.println("Devision is :"+ l);
-		out.println("Multiplication is :"+ m);
-		
-	
+		res.sendRedirect("Sq");
 
-		
-//		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
-//		rd.forward(req, res);
-		
-		
-		
-		
+ 
 		
 	}
 	
--- src/com/Adore96/SqServlet.java
+++ src/com/Adore96/SqServlet.java
@@ -3,19 +3,20 @@ package com.Adore96;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SqServlet extends HttpServlet {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-	public void doPost(HttpServletResponse res , HttpServletRequest req) throws IOException {
-		PrintWriter out = res.getWriter();
-		out.println("Check");
+	public void doGet(HttpServletRequest req , HttpServletResponse res) throws IOException{
+		System.out.println("SQ called");
+//		PrintWriter out = res.getWriter();
+//		out.println("Check");
+		
+//		RequestDispatcher rd = req.getRequestDispatcher("index.html");
+//		rd.forward(req, res);
 	}
 }
